4999d405c6cce0a9d31818a76efc155f2baa265c
Bruno Zabala <brunozabalap@gmail.com>
2026-01-28 19:20:43 -0600

    Add PhotoController and model

---
+++ api-BPhotos/Controllers/PhotoController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using api_BPhotos.Models;
+
+namespace api_BPhotos.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PhotoController : ControllerBase
+    {
+        //private readonly string _storagePath = "";
+        private readonly string _storagePath = "";
+
+
+        [HttpGet]
+        public IEnumerable<Photo> GetLocal()
+        {
+            var photos = new List<Photo>();
+            var imageFiles = Directory.GetFiles(_storagePath, "*.*", SearchOption.TopDirectoryOnly)
+                                      .Where(file => file.EndsWith(".jpg", StringComparison.OrdinalIgnoreCase) ||
+                                                     file.EndsWith(".jpeg", StringComparison.OrdinalIgnoreCase) ||
+                                                     file.EndsWith(".png", StringComparison.OrdinalIgnoreCase) ||
+                                                     file.EndsWith(".gif", StringComparison.OrdinalIgnoreCase) ||
+                                                     file.EndsWith(".webp", StringComparison.OrdinalIgnoreCase));
+            foreach (var filePath in imageFiles)
+            {
+                var fileInfo = new FileInfo(filePath);
+                photos.Add(new Photo
+                {
+                    Title = Path.GetFileNameWithoutExtension(filePath),
+                    ImageURL = $"test/url",
+                    DateTaken = fileInfo.CreationTime
+                });
+            }
+            return photos;
+        }
+    }
+}
---
+++ api-BPhotos/Models/Photo.cs
@@ -0,0 +1,9 @@
+﻿namespace api_BPhotos.Models
+{
+    public class Photo
+    {
+        public required string Title { get; set; }
+        public required string ImageURL { get; set; }
+        public required DateTime DateTaken { get; set; }
+    } 
+}
