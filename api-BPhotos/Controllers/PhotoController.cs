880242c79b8fb665b432f18b438fa3d1f2ab0583
Bruno Zabala <brunozabalap@gmail.com>
2026-01-29 21:41:37 -0600

    Endpoints to serve original image and thumbnail (ImageSharp)

--- api-BPhotos/Controllers/PhotoController.cs
+++ api-BPhotos/Controllers/PhotoController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using api_BPhotos.Models;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
 
 namespace api_BPhotos.Controllers
 {
@@ -26,12 +28,62 @@ namespace api_BPhotos.Controllers
                 var fileInfo = new FileInfo(filePath);
                 photos.Add(new Photo
                 {
-                    Title = Path.GetFileNameWithoutExtension(filePath),
-                    ImageURL = $"test/url",
+                    Title = Path.GetFileName(filePath),
+                    ImageURL = $"http://ip:port/api/photo/show/{Path.GetFileName(filePath)}",
+                    ImageThumbnailURL = $"http://ip:port/api/photo/thumbnail/{Path.GetFileName(filePath)}",
                     DateTaken = fileInfo.CreationTime
                 });
             }
             return photos;
+        }
+
+        [HttpGet("thumbnail/{title}")]
+        public async Task<IActionResult> GetThumbnail(string title)
+        {
+            var filePath = Path.Combine(_storagePath, title);
+            if (!System.IO.File.Exists(filePath))
+            {
+                return NotFound();
+            }
+
+            var thumbnailStream = new MemoryStream();
+
+            using (var image = await Image.LoadAsync(filePath))
+            {
+                image.Mutate(x => x.Resize(new ResizeOptions
+                {
+                    Size = new Size(300, 300),
+                    Mode = ResizeMode.Max
+                }));
+                await image.SaveAsJpegAsync(thumbnailStream);
+            }
+
+            thumbnailStream.Position = 0;
+
+            return File(thumbnailStream, "image/jpeg");
+        }
+
+        [HttpGet("show/{title}")]
+        public IActionResult ShowImage(string title)
+        {
+            var filePath = Path.Combine(_storagePath, title);
+            if (!System.IO.File.Exists(filePath))
+            {
+                return NotFound();
+            }
+            var extension = Path.GetExtension(filePath).ToLower();
+            var imageStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+
+            string contentType = extension switch
+            {
+                ".jpg" or ".jpeg" => "image/jpeg",
+                ".png" => "image/png",
+                ".gif" => "image/gif",
+                ".webp" => "image/webp",
+                _ => "application/octet-stream",
+            };
+
+            return File(imageStream, contentType);
         }
     }
 }
--- api-BPhotos/Models/Photo.cs
+++ api-BPhotos/Models/Photo.cs
@@ -4,6 +4,7 @@
     {
         public required string Title { get; set; }
         public required string ImageURL { get; set; }
+        public required string ImageThumbnailURL { get; set; }
         public required DateTime DateTaken { get; set; }
     } 
 }
